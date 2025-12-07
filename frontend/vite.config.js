6a6c92232613dc64c92ab1f631a2ad9813c727de
Vishal Sale <105708248+VishalSale@users.noreply.github.com>
2025-12-07 13:20:13 +0530

    Fix router for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.MODE === 'production' ? '/school-project/' : '/'
+  base: mode === 'production' ? '/school-project/' : '/'
 })
